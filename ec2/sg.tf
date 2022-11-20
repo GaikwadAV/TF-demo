a36aae84842cd028ce9baef0ab3132c77afc84ac
AniketGaikwad <aniket.gaikwad@gslab.com>
2022-11-20 16:12:07 +0530

    ec2-all

---
+++ ec2/sg.tf
@@ -0,0 +1,29 @@
+# provider "aws" {
+#   region = "ap-south-1"
+# }
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow port 22"
+  description = "Allow ssh inbound traffic"
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ ec2/variables.tf
@@ -0,0 +1,6 @@
+variable "ami" {
+  default = "ami-026b57f3c383c2eec"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
